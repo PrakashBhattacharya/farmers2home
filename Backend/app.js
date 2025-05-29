124f6daac7f850d13f0bff080791b234d58e1e1a
Lakshya yadav <yadavlakshya743@gmail.com>
2025-05-29 23:38:32 +0530

    Prepared backend for Vercel deployment

--- Backend/app.js
+++ Backend/app.js
@@ -5,21 +5,22 @@ const userRoutes = require('./Router/User');
 const productRoutes = require('./Router/Product');
 const orderRoutes = require('./Router/Order');
 const cors = require('cors');
+const serverless = require('serverless-http'); 
 
 const app = express();
 
+// Connect to MongoDB
 connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the homepage!');
 });
-app.use(express.json());
-app.use(cors()); 
-
 app.use('/api/auth', userRoutes);
 app.use('/api/products', productRoutes); 
 app.use('/api/orders', orderRoutes); 
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+module.exports = serverless(app);
